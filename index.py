eb025be9d3527e748d2df9426de09d364fd4f451
Acen1991 <mohamedhassen-mami@hotmail.fr>
2015-03-19 11:59:45 +0100

    fixing the problem of reading cropped file multiple times

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import tesseract
 import numpy
 import sys
 import os
+import re
 
 def detect_words(filename):
 	api = tesseract.TessBaseAPI()
@@ -17,22 +18,32 @@ def detect_words(filename):
 
 def crop_image(filename):
 	img = cv2.imread(filename)
-	name_board = img[200:225, 130:350]
-	topic_board = img[220:400, 0:350]
+	name_board = img[450:500, 290:720]
+	topic_board = img[495:576, 0:720]
+	cv2.imwrite(filename+'_name.jpg', name_board)
+	cv2.imwrite(filename+'_topic.jpg', topic_board)
+
+def clean_cropped_images():
+	dir = '.'
+	for f in os.listdir(dir):
+		if re.search('_(name|topic)\.jpg', f):
+			os.remove(os.path.join(dir, f))
 
 def main():
+	clean_cropped_images()
 
-	extensionsToCheck = (".png",".jpg",".tiff")
+	orginal_files_extensions = (".png",".jpg",".tiff")
+	cropped_files_extensions = ("_name.jpg","_topic.jpg")
 
 	filenames = next(os.walk('.'))[2]
 
 	for filename in filenames:
-		if filename.endswith(extensionsToCheck):
+		if filename.endswith(orginal_files_extensions):
 			crop_image(filename)
 
 	for filename in filenames:
-		if filename.endswith(extensionsToCheck):
+		if filename.endswith(cropped_files_extensions):
 			print filename,':'
-			print detectWords(filename)
+			print detect_words(filename)
 			print '-'*100
 main()
