314ffd7b594ce862788d5b8c284ce6c3a7a127eb
Acen1991 <mohamedhassen-mami@hotmail.fr>
2015-03-20 12:20:11 +0100

    refactoring

--- index.py
+++ index.py
@@ -4,106 +4,102 @@ import numpy
 import sys
 import os
 import re
+import csv
+from datetime import timedelta, date
+import time
 
-def detect_words(filename):
+def detect_words(filename, left, top, width, height):
 	api = tesseract.TessBaseAPI()
 	api.SetOutputName("outputName");
 	api.Init(".","fra",tesseract.OEM_DEFAULT)
 	api.SetPageSegMode(tesseract.PSM_AUTO)
+	
 	pixImage=tesseract.pixRead(filename)
+	
 	api.SetImage(pixImage)
+	api.SetRectangle(left,top,width,height)
 	outText=api.GetUTF8Text()
 	api.End()
+
 	return outText
 
-def crop_image(filename):
-	img = cv2.imread('frames.avi/'+filename)
-	name_board = img[450:500, 290:720]
-	topic_board = img[495:576, 0:720]
-	
-	cv2.imwrite('frames_cropped/'+filename+'_name.jpg', name_board)
-	cv2.imwrite('frames_cropped/'+filename+'_topic.jpg', topic_board)
+def image_processing_with_threshold(filename):
+	image = cv2.imread(filename)
+	ret, threshold = cv2.threshold(image, 225,255, cv2.THRESH_BINARY_INV)
+	cv2.imwrite(filename.replace(".jpg","")+"_thresholded.jpg", threshold)
 
-def crop_image_name_function(filename):
-	img = cv2.imread('frames.avi/'+filename)
-	name_board = img[450:480, 290:720]
-	function_board = img[475:500, 290:720]
-	
-	cv2.imwrite('frames_cropped/'+filename+'_name.jpg', name_board)
-	cv2.imwrite('frames_cropped/'+filename+'_function.jpg', function_board)
-	
+def run(startSecond, frame_rate):
+	with open("names_annotations.csv","wb") as names_csv:
+		with open("functions_annotations.csv","wb") as functions_csv:
+			with open("topics_annotations.csv","wb") as topics_csv:			
+				names_writer = csv.writer(names_csv, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+				functions_writer = csv.writer(functions_csv, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+				topics_writer = csv.writer(topics_csv, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+					
+				names_array = []
+				topics_array = []
+				functions_array = []
 
-def clean_cropped_images(dir):
-	for f in os.listdir(dir):
-		if re.search('_(name|topic|function)\.jpg', f):
-			os.remove(os.path.join(dir, f))
+				orginal_files_extensions = (".png",".jpg",".tiff")
+				cropped_files_extensions = ("_name.jpg","_topic.jpg")
+				name_files_extension = "_name.jpg"
+				topic_files_extension = "_topic.jpg"
+				function_files_extension = "_function.jpg"
+
+				frames_folder = next(os.walk("frames.avi"))[2]
+
+				for filename in frames_folder:
+					image_processing_with_threshold('frames.avi/'+filename)
+
+				for i in range(0, len(frames_folder)):
+					filename = frames_folder[i]
+					duration = 3
+					current_time_stamp =  startSecond+i*frame_rate
+					start_time = timedelta(0, current_time_stamp)
+					stop_time = timedelta(0, current_time_stamp+duration)
+					duration = timedelta(0, duration)
+					
+
+					print filename,current_time_stamp
+					
+					current_name = detect_words('frames.avi/'+filename.replace(".jpg","")+"_thresholded.jpg", 290, 450, 720-290, 500-450)
+					current_function = ''
+					current_topic = detect_words('frames.avi/'+filename, 0, 495, 720, 576-495)
+
+					if '\n' in current_name:
+						current_name = detect_words('frames.avi/'+filename.replace(".jpg","")+"_thresholded.jpg", 290, 450, 720-290, 480-450,)
+						current_function = detect_words('frames.avi/'+filename.replace(".jpg","")+"_thresholded.jpg", 290, 475, 720-290, 500-475)
+
+					if current_name.strip() != "":
+						print 'OCR_NAME', current_name.strip(), start_time, stop_time, duration
+						names_writer.writerow(['OCR_NAME', current_name.strip(), start_time, stop_time, duration])
+						names_array.append(current_name)
+					
+					if current_function.strip() != "":
+						print 'OCR_FUNCTION', current_function.strip(), start_time, stop_time, duration
+						functions_writer.writerow(['OCR_FUNCTION', current_function.strip(), start_time, stop_time, duration])
+						functions_array.append(current_function)
+					
+					if current_topic.strip() != "":
+						print 'OCR_TOPIC', current_topic.strip(), start_time, stop_time, duration
+						topics_writer.writerow(['OCR_TOPIC', current_topic.strip(), start_time, stop_time, duration])
+						topics_array.append(current_topic)
+				
+				print names_array
+
+				print '\n'
+
+				print topics_array
+
+				print '\n'
+
+				print functions_array
+
+				print '\n'
+
+				print len(names_array), len(functions_array), len(topics_array)
 
 def main():
-	startMillisecond = 500
-
-	clean_cropped_images("frames_cropped")
-
-	names_buffer = open('names_buffer.txt', 'w')
-	topics_buffer = open('topics_buffer.txt', 'w')
-	functions_buffer = open('functions_buffer.txt', 'w')
-
-	names_array = []
-	topics_array = []
-	functions_array = []
-
-	orginal_files_extensions = (".png",".jpg",".tiff")
-	cropped_files_extensions = ("_name.jpg","_topic.jpg")
-	name_files_extension = "_name.jpg"
-	topic_files_extension = "_topic.jpg"
-	function_files_extension = "_function.jpg"
-
-	frames_folder = next(os.walk("frames.avi"))[2]
-
-	for filename in frames_folder:
-		if filename.endswith(orginal_files_extensions):
-			crop_image(filename)
-
-	
-	cropped_folder = next(os.walk("frames_cropped"))[2]
-
-	
-	for filename in cropped_folder:
-		if filename.endswith(name_files_extension):
-			current_name = detect_words('frames_cropped/'+filename)
-			current_function = ''
-
-			if '\n' in current_name:
-				crop_image_name_function(filename.replace("_name.jpg",""))
-				current_name = detect_words('frames_cropped/'+filename)
-				current_function = detect_words('frames_cropped/'+filename.replace("_name.jpg","")+function_files_extension)
-
-			names_buffer.write(current_name+'\n')
-			functions_buffer.write(current_function+'\n')
-			
-			names_array.append(current_name)
-			functions_array.append(current_function)
-
-		elif filename.endswith(topic_files_extension):
-			current_topic = detect_words('frames_cropped/'+filename)
-			topics_array.append(current_topic)
-			topics_buffer.write(current_topic+'\n')
-
-	names_buffer.close()
-	functions_buffer.close()
-	topics_buffer.close()
-
-	print names_array
-
-	print '\n'
-
-	print topics_array
-
-	print '\n'
-
-	print functions_array
-
-	print '\n'
-
-	print len(names_array), len(functions_array), len(topics_array)
+	run(263, 3)
 
 main()
